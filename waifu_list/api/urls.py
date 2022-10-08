f0cd2aed7cd42744ea23dde1990c12557c341bad
Charles Yuan <charlesyuan59@gmail.com>
2022-10-08 16:35:30 -0400

    Add endpoint for get and delete all

--- waifu_list/api/urls.py
+++ waifu_list/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("", index, name="index"),
     path('api', WaifuApiView.as_view()),
-    path('api/all', WaifuApiGetAll.as_view()),
+    path('api/all', WaifuApiAll.as_view()),
 ]
